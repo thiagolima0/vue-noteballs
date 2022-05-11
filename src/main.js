b729f5ab194c51860d69d5c739e2ae8b33318780
thiagolima_ <thiago.lima@toutsolucoes.com.br>
2022-05-11 00:36:32 -0300

    tidying up routes

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 import App from "./App.vue";
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [],
-});
+import router from "@/router"
 
 createApp(App).use(router).mount("#app");
