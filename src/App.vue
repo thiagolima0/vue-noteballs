29527297de12f11034c75ce1246f4788909e458b
thiagolima_ <thiago.lima@toutsolucoes.com.br>
2022-05-11 01:16:09 -0300

    bulma ready

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink> | 
-  <RouterLink to="/stats">Stats</RouterLink>
-
-  <RouterView />
+  <NavBar />
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>
 </template>
+
+<script setup>
+import NavBar from "./components/Layout/NavBar.vue";
+</script>
+
+<style>
+@import "bulma/css/bulma.min.css";
+</style>
---
+++ src/components/Layout/NavBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <nav class="navbar is-success" role="navigation" aria-label="main navigation">
+    <div class="container is-max-desktop">
+      <div class="navbar-brand">
+        <div class="navbar-item is-size-4 ml-2 is-family-monospace">Noteballs</div>
+
+        <a
+          @click.prevent="showMenu = !showMenu"
+          role="button"
+          class="navbar-burger"
+          :class="{ 'is-active': showMenu }"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+
+      <div
+        id="navbarBasicExample"
+        class="navbar-menu"
+        :class="{ 'is-active': showMenu }"
+      >
+        <div class="navbar-end">
+          <RouterLink to="/" class="navbar-item" active-class="is-active">
+            Notes
+          </RouterLink>
+
+          <RouterLink to="/stats" class="navbar-item" active-class="is-active">
+            Stats
+          </RouterLink>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const showMenu = ref(false);
+</script>
+
+<style scoped>
+@media (max-width: 1023px) {
+  .navbar-menu {
+    position: absolute;
+    width: 100%;
+    left: 0;
+  }
+}
+</style>
